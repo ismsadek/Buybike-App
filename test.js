4cdde2b66e365044096723fc55e59dd1477f8f2c
unknown <ismsadek@gmail.com>
2018-05-14 23:05:23 +0200

    Testing

---
+++ test.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+
+let scrape = async () => {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+
+    await page.goto('http://books.toscrape.com/');
+
+    const result = await page.evaluate(() => {
+        let data = []; // Create an empty array that will store our data
+        let elements = document.querySelectorAll('.product_pod'); // Select all Products
+
+        for (var element of elements){ // Loop through each proudct
+            let title = element.childNodes[5].innerText; // Select the title
+            let price = element.childNodes[7].children[0].innerText; // Select the price
+
+            data.push({title, price}); // Push an object with the data onto our array
+        }
+
+        return data; // Return our data array
+    });
+
+    browser.close();
+    return result; // Return the data
+};
+
+scrape().then((value) => {
+    console.log(value); // Success!
+});
+
